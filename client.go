b402d96f1d0fd089a31a0db6184bd0f61066f918
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-10-24 18:37:34 -0700

    support all major checksum types and add tests

--- client.go
+++ client.go
@@ -1,14 +1,19 @@
 package getter
 
 import (
+	"bytes"
+	"crypto/md5"
+	"crypto/sha1"
 	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/hex"
 	"fmt"
+	"hash"
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	urlhelper "github.com/hashicorp/terraform/helper/url"
 )
@@ -75,25 +80,63 @@ func (c *Client) Get() error {
 			"download not supported for scheme '%s'", force)
 	}
 
-	var sum string
-	sumRaw := u.Query()["checksum"]
-	if len(sumRaw) == 1 {
-		sum = sumRaw[0]
+	// Determine if we have a checksum
+	var checksumHash hash.Hash
+	var checksumValue []byte
+	if v := u.Query().Get("checksum"); v != "" {
+		// Delete the query parameter if we have it.
+		u.Query().Del("checksum")
+
+		// If we're getting a directory, then this is an error. You cannot
+		// checksum a directory. TODO: test
+		if c.Dir {
+			return fmt.Errorf(
+				"checksum cannot be specified for directory download")
+		}
+
+		// Determine the checksum hash type
+		checksumType := ""
+		idx := strings.Index(v, ":")
+		if idx > -1 {
+			checksumType = v[:idx]
+		}
+		switch checksumType {
+		case "md5":
+			checksumHash = md5.New()
+		case "sha1":
+			checksumHash = sha1.New()
+		case "sha256":
+			checksumHash = sha256.New()
+		case "sha512":
+			checksumHash = sha512.New()
+		default:
+			return fmt.Errorf(
+				"unsupported checksum type: %s", checksumType)
+		}
+
+		// Get the remainder of the value and parse it into bytes
+		b, err := hex.DecodeString(v[idx+1:])
+		if err != nil {
+			return fmt.Errorf("invalid checksum: %s", err)
+		}
+
+		// Set our value
+		checksumValue = b
 	}
 
 	// If we're not downloading a directory, then just download the file
 	// and return.
 	if !c.Dir {
-		if sum == "" {
-			return g.GetFile(dst, u)
-		}
-
 		err := g.GetFile(dst, u)
 		if err != nil {
 			return err
 		}
 
-		return checksum(dst, sum)
+		if checksumHash != nil {
+			return checksum(dst, checksumHash, checksumValue)
+		}
+
+		return nil
 	}
 
 	// We're downloading a directory, which might require a bit more work
@@ -116,32 +159,27 @@ func (c *Client) Get() error {
 		return copyDir(realDst, filepath.Join(dst, subDir), false)
 	}
 
-	return checksum(realDst, sum)
+	return nil
 }
 
-// checksum is a simple method to compute the SHA256 checksum of a source (file
-// or dir) and compare it to a given sum.
-func checksum(source, sum string) error {
-	if sum == "" {
-		return nil
-	}
-	// compute and check checksum
-	log.Printf("[DEBUG] Running checksum on (%s)", source)
-	hasher := sha256.New()
-	file, err := os.Open(source)
+// checksum is a simple method to compute the checksum of a source file
+// and compare it to the given expected value.
+func checksum(source string, h hash.Hash, v []byte) error {
+	f, err := os.Open(source)
 	if err != nil {
 		return fmt.Errorf("Failed to open file for checksum: %s", err)
 	}
+	defer f.Close()
 
-	defer file.Close()
-	io.Copy(hasher, file)
+	if _, err := io.Copy(h, f); err != nil {
+		return fmt.Errorf("Failed to hash: %s", err)
+	}
 
-	computed := hex.EncodeToString(hasher.Sum(nil))
-	if sum != computed {
+	if actual := h.Sum(nil); !bytes.Equal(actual, v) {
 		return fmt.Errorf(
 			"Checksums did not match.\nExpected (%s), got (%s)",
-			sum,
-			computed)
+			hex.EncodeToString(v),
+			hex.EncodeToString(actual))
 	}
 
 	return nil
