c4d136678f7e3f25236629c54be8e59e664e8508
Adrien Delorme <adrien.delorme@icloud.com>
2018-11-20 00:01:59 +0100

    allow bin mode to gracefully cancel a download

--- cmd/go-getter/main.go
+++ cmd/go-getter/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"os"
+	"os/signal"
+	"sync"
 
 	"github.com/hashicorp/go-getter"
 )
@@ -38,25 +41,49 @@ func main() {
 		log.Fatalf("Error getting wd: %s", err)
 	}
 
-	// Build the client
-	client := &getter.Client{
-		Src:  args[0],
-		Dst:  args[1],
-		Pwd:  pwd,
-		Mode: mode,
+	ctx, cancel := context.WithCancel(context.Background())
+	opts := []getter.ClientOption{
+		getter.WithContext(ctx),
 	}
-	var opts []getter.ClientOption
 	if *progress {
 		opts = append(opts, getter.WithProgress(defaultProgressBar))
 	}
 
-	if err := client.Configure(opts...); err != nil {
-		log.Fatalf("Configure: %s", err)
+	// Build the client
+	client := &getter.Client{
+		Src:     args[0],
+		Dst:     args[1],
+		Pwd:     pwd,
+		Mode:    mode,
+		Ctx:     ctx,
+		Options: opts,
 	}
 
-	if err := client.Get(); err != nil {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	errChan := make(chan error, 2)
+	go func() {
+		defer wg.Done()
+		defer cancel()
+		if err := client.Get(); err != nil {
+			errChan <- err
+		}
+	}()
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+
+	select {
+	case sig := <-c:
+		signal.Reset(os.Interrupt)
+		cancel()
+		wg.Wait()
+		log.Printf("signal %v", sig)
+	case <-ctx.Done():
+		wg.Wait()
+		log.Printf("success!")
+	case err := <-errChan:
+		wg.Wait()
 		log.Fatalf("Error downloading: %s", err)
 	}
-
-	log.Println("Success!")
 }
