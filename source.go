a6260be074ddb7db0246224d213cef605a97ef27
Adrien Delorme <adrien.delorme@icloud.com>
2018-12-19 16:27:38 +0100

    remove named parameters of SourceDirSubdir and document it with examples instead
    * also avoid protocol to be matched from the query parameters

--- source.go
+++ source.go
@@ -6,20 +6,27 @@ import (
 	"strings"
 )
 
-// SourceDirSubdir takes a source and returns the URL without
+// SourceDirSubdir takes a source URL and returns a tuple of the URL without
 // the subdir and the subdir.
-func SourceDirSubdir(src string) (url string, subdir string) {
-	// Calcaulate an offset to avoid accidentally marking the scheme
-	// as the dir.
-	var offset int
-	if idx := strings.Index(src, "://"); idx > -1 {
-		offset = idx + 3
-	}
+//
+// ex:
+//   dom.com/path/?q=p               => dom.com/path/?q=p, ""
+//   proto://dom.com/path//*?q=p     => proto://dom.com/path?q=p, "*"
+//   proto://dom.com/path//path2?q=p => proto://dom.com/path?q=p, "path2"
+//
+func SourceDirSubdir(src string) (string, string) {
 
 	// URL might contains another url in query parameters
 	stop := len(src)
 	if idx := strings.Index(src, "?"); idx > -1 {
 		stop = idx
+	}
+
+	// Calculate an offset to avoid accidentally marking the scheme
+	// as the dir.
+	var offset int
+	if idx := strings.Index(src[:stop], "://"); idx > -1 {
+		offset = idx + 3
 	}
 
 	// First see if we even have an explicit subdir
@@ -29,7 +36,7 @@ func SourceDirSubdir(src string) (url string, subdir string) {
 	}
 
 	idx += offset
-	subdir = src[idx+2:]
+	subdir := src[idx+2:]
 	src = src[:idx]
 
 	// Next, check if we have query parameters and push them onto the
