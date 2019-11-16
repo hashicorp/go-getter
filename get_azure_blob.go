1c0007dfc50562d5e4ad2951e693b62fea57bdc6
Jared Dickson <jdickson@ostusa.com>
2019-11-16 01:35:08 -0500

    WIP build is working

--- get_azure_blob.go
+++ get_azure_blob.go
@@ -2,131 +2,191 @@ package getter
 
 import (
 	"fmt"
-	"io"
+	//"io"
 	"net/url"
-	"os"
-	"path/filepath"
+	//"os"
+	//"path/filepath"
 	"strings"
-
-	storage "github.com/Azure/azure-storage-go"
+	//"log"
+	//
+	"github.com/Azure/azure-storage-blob-go/azblob"
 )
+// TODO: https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-go?tabs=windows#understand-the-sample-code
+
 
 // AzureBlobGetter is a Getter implementation that will download a module from
 // an Azure Blob Storage Account.
-type AzureBlobGetter struct{}
+type AzureBlobGetter struct {
+	getter
+}
 
 func (g *AzureBlobGetter) ClientMode(u *url.URL) (ClientMode, error) {
-	// Parse URL
-	accountName, baseURL, containerName, blobPath, accessKey, err := g.parseUrl(u)
-	if err != nil {
-		return 0, err
-	}
+	//// Parse URL
+	//accountName, baseURL, containerName, blobPath, accessKey, err := g.parseUrl(u)
+	//if err != nil {
+	//	return 0, err
+	//}
+	//
+	//client, err := g.getBobClient(accountName, baseURL, accessKey)
+	//if err != nil {
+	//	return 0, err
+	//}
+	//
+	//container := client.GetContainerReference(containerName)
+	//
+	//containerReference := storage.GetContainerReference(containerName)
+	//blobReference := containerReference.GetBlobReference(c.keyName)
+	//options := &storage.GetBlobOptions{}
+	//
+	//// List the object(s) at the given prefix
+	//params := storage.ListBlobsParameters{
+	//	Prefix: blobPath,
+	//}
+	//resp, err := container.ListBlobs(params)
+	//if err != nil {
+	//	return 0, err
+	//}
+	//
+	//for _, b := range resp.Blobs {
+	//	// Use file mode on exact match.
+	//	if b.Name == blobPath {
+	//		return ClientModeFile, nil
+	//	}
+	//
+	//	// Use dir mode if child keys are found.
+	//	if strings.HasPrefix(b.Name, blobPath+"/") {
+	//		return ClientModeDir, nil
+	//	}
+	//}
+	//
+	//// There was no match, so just return file mode. The download is going
+	//// to fail but we will let Azure return the proper error later.
+	//return ClientModeFile, nil
+	//ClientModeFile := nil
 
-	client, err := g.getBobClient(accountName, baseURL, accessKey)
-	if err != nil {
-		return 0, err
-	}
+	// From the Azure portal, get your storage account name and key and set environment variables.
+	//accountName, accountKey := os.Getenv("AZURE_STORAGE_ACCOUNT"), os.Getenv("AZURE_STORAGE_ACCESS_KEY")
+	//if len(accountName) == 0 || len(accountKey) == 0 {
+	//	log.Fatal("Either the AZURE_STORAGE_ACCOUNT or AZURE_STORAGE_ACCESS_KEY environment variable is not set")
+	//}
+	//
+	//// Create a default request pipeline using your storage account name and account key.
+	//credential, err := azblob.NewSharedKeyCredential(accountName, accountKey)
+	//if err != nil {
+	//	log.Fatal("Invalid credentials with error: " + err.Error())
+	//}
+	//p := azblob.NewPipeline(credential, azblob.PipelineOptions{})
+	//
+	//// Create a random string for the quick start container
+	//containerName := fmt.Sprintf("quickstart-%s", randomString())
+	//
+	//// From the Azure portal, get your storage account blob service URL endpoint.
+	//URL, _ := url.Parse(
+	//	fmt.Sprintf("https://%s.blob.core.windows.net/%s", accountName, containerName))
+	//
+	//// Create a ContainerURL object that wraps the container URL and a request
+	//// pipeline to make requests.
+	//containerURL := azblob.NewContainerURL(*URL, p)
+	//
+	//// Create the container
+	//fmt.Printf("Creating a container named %s\n", containerName)
+	//ctx := context.Background() // This example uses a never-expiring context
+	//_, err = containerURL.Create(ctx, azblob.Metadata{}, azblob.PublicAccessNone)
+	//handleErrors(err)
+	//
+	//
+	//
+	//
+	//// Here's how to download the blob
+	//downloadResponse, err := blobURL.Download(ctx, 0, azblob.CountToEnd, azblob.BlobAccessConditions{}, false)
+	//
+	//// NOTE: automatically retries are performed if the connection fails
+	//bodyStream := downloadResponse.Body(azblob.RetryReaderOptions{MaxRetryRequests: 20})
+	//
+	//// read the body into a buffer
+	//downloadedData := bytes.Buffer{}
+	//_, err = downloadedData.ReadFrom(bodyStream)
+	//handleErrors(err)
 
-	container := client.GetContainerReference(containerName)
 
-	// List the object(s) at the given prefix
-	params := storage.ListBlobsParameters{
-		Prefix: blobPath,
-	}
-	resp, err := container.ListBlobs(params)
-	if err != nil {
-		return 0, err
-	}
 
-	for _, b := range resp.Blobs {
-		// Use file mode on exact match.
-		if b.Name == blobPath {
-			return ClientModeFile, nil
-		}
 
-		// Use dir mode if child keys are found.
-		if strings.HasPrefix(b.Name, blobPath+"/") {
-			return ClientModeDir, nil
-		}
-	}
 
-	// There was no match, so just return file mode. The download is going
-	// to fail but we will let Azure return the proper error later.
 	return ClientModeFile, nil
 }
 
 func (g *AzureBlobGetter) Get(dst string, u *url.URL) error {
 	// Parse URL
-	accountName, baseURL, containerName, blobPath, accessKey, err := g.parseUrl(u)
-	if err != nil {
-		return err
-	}
-
-	// Remove destination if it already exists
-	_, err = os.Stat(dst)
-	if err != nil && !os.IsNotExist(err) {
-		return err
-	}
-
-	if err == nil {
-		// Remove the destination
-		if err := os.RemoveAll(dst); err != nil {
-			return err
-		}
-	}
-
-	// Create all the parent directories
-	if err := os.MkdirAll(filepath.Dir(dst), 0755); err != nil {
-		return err
-	}
-
-	client, err := g.getBobClient(accountName, baseURL, accessKey)
-	if err != nil {
-		return err
-	}
-
-	container := client.GetContainerReference(containerName)
-
-	// List files in path, keep listing until no more objects are found
-	lastMarker := ""
-	hasMore := true
-	for hasMore {
-		params := storage.ListBlobsParameters{
-			Prefix: blobPath,
-		}
-		if lastMarker != "" {
-			params.Marker = lastMarker
-		}
-
-		resp, err := container.ListBlobs(params)
-		if err != nil {
-			return err
-		}
-
-		hasMore = resp.NextMarker != ""
-		lastMarker = resp.NextMarker
-
-		// Get each object storing each file relative to the destination path
-		for _, object := range resp.Blobs {
-			objPath := object.Name
-
-			// If the key ends with a backslash assume it is a directory and ignore
-			if strings.HasSuffix(objPath, "/") {
-				continue
-			}
-
-			// Get the object destination path
-			objDst, err := filepath.Rel(blobPath, objPath)
-			if err != nil {
-				return err
-			}
-			objDst = filepath.Join(dst, objDst)
-
-			if err := g.getObject(client, objDst, containerName, objPath); err != nil {
-				return err
-			}
-		}
-	}
+	//accountName, baseURL, containerName, blobPath, accessKey, err := g.parseUrl(u)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//// Remove destination if it already exists
+	//_, err = os.Stat(dst)
+	//if err != nil && !os.IsNotExist(err) {
+	//	return err
+	//}
+	//
+	//if err == nil {
+	//	// Remove the destination
+	//	if err := os.RemoveAll(dst); err != nil {
+	//		return err
+	//	}
+	//}
+	//
+	//// Create all the parent directories
+	//if err := os.MkdirAll(filepath.Dir(dst), 0755); err != nil {
+	//	return err
+	//}
+	//
+	//client, err := g.getBobClient(accountName, baseURL, accessKey)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//container := client.GetContainerReference(containerName)
+	//
+	//// List files in path, keep listing until no more objects are found
+	//lastMarker := ""
+	//hasMore := true
+	//for hasMore {
+	//	params := storage.ListBlobsParameters{
+	//		Prefix: blobPath,
+	//	}
+	//	if lastMarker != "" {
+	//		params.Marker = lastMarker
+	//	}
+	//
+	//	resp, err := container.ListBlobs(params)
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	hasMore = resp.NextMarker != ""
+	//	lastMarker = resp.NextMarker
+	//
+	//	// Get each object storing each file relative to the destination path
+	//	for _, object := range resp.Blobs {
+	//		objPath := object.Name
+	//
+	//		// If the key ends with a backslash assume it is a directory and ignore
+	//		if strings.HasSuffix(objPath, "/") {
+	//			continue
+	//		}
+	//
+	//		// Get the object destination path
+	//		objDst, err := filepath.Rel(blobPath, objPath)
+	//		if err != nil {
+	//			return err
+	//		}
+	//		objDst = filepath.Join(dst, objDst)
+	//
+	//		if err := g.getObject(client, objDst, containerName, objPath); err != nil {
+	//			return err
+	//		}
+	//	}
+	//}
 
 	return nil
 }
@@ -145,41 +205,43 @@ func (g *AzureBlobGetter) GetFile(dst string, u *url.URL) error {
 	return g.getObject(client, dst, containerName, blobPath)
 }
 
-func (g *AzureBlobGetter) getObject(client storage.BlobStorageClient, dst, container, blobName string) error {
-	r, err := client.GetBlob(container, blobName)
-	if err != nil {
-		return err
-	}
-
-	// Create all the parent directories
-	if err := os.MkdirAll(filepath.Dir(dst), 0755); err != nil {
-		return err
-	}
-
-	f, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	_, err = io.Copy(f, r)
-	return err
+func (g *AzureBlobGetter) getObject(client *azblob.SharedKeyCredential, dst, container, blobName string) error {
+	//r, err := client(container, blobName)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//// Create all the parent directories
+	//if err := os.MkdirAll(filepath.Dir(dst), 0755); err != nil {
+	//	return err
+	//}
+	//
+	//f, err := os.Create(dst)
+	//if err != nil {
+	//	return err
+	//}
+	//defer f.Close()
+	//
+	//_, err = io.Copy(f, r)
+	//return err
+	return nil
 }
 
-func (g *AzureBlobGetter) getBobClient(accountName string, baseURL string, accountKey string) (storage.BlobStorageClient, error) {
-	var b storage.BlobStorageClient
-
-	if accountKey == "" {
-		accountKey = os.Getenv("ARM_ACCESS_KEY")
-	}
-
-	c, err := storage.NewClient(accountName, accountKey, baseURL, storage.DefaultAPIVersion, true)
-	if err != nil {
-		return b, err
-	}
-
-	b = c.GetBlobService()
-
+func (g *AzureBlobGetter) getBobClient(accountName string, baseURL string, accountKey string) (*azblob.SharedKeyCredential, error) {
+	var b *azblob.SharedKeyCredential
+	//
+	//if accountKey == "" {
+	//	accountKey = os.Getenv("ARM_ACCESS_KEY")
+	//}
+	//
+	//c, err := storage.NewClient(accountName, accountKey, baseURL, storage.DefaultAPIVersion, true)
+	//if err != nil {
+	//	return b, err
+	//}
+	//
+	//b = c.GetBlobService()
+	//
+	//return b, nil
 	return b, nil
 }
 
--- go.mod
+++ go.mod
@@ -1,26 +1,41 @@
 module github.com/hashicorp/go-getter
 
 require (
-	cloud.google.com/go v0.45.1
+	cloud.google.com/go v0.48.0
+	cloud.google.com/go/storage v1.3.0
+	github.com/Azure/azure-pipeline-go v0.2.2 // indirect
+	github.com/Azure/azure-storage-blob-go v0.8.0
 	github.com/Azure/go-autorest/autorest v0.9.2
-	github.com/aws/aws-sdk-go v1.15.78
+	github.com/Azure/go-autorest/autorest/adal v0.8.0 // indirect
+	github.com/Azure/go-autorest/autorest/to v0.3.0 // indirect
+	github.com/aws/aws-sdk-go v1.25.34
 	github.com/bgentry/go-netrc v0.0.0-20140422174119-9fd32a8b3d3d
 	github.com/cheggaaa/pb v1.0.27
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dnaeon/go-vcr v1.0.1 // indirect
 	github.com/fatih/color v1.7.0 // indirect
-	github.com/hashicorp/go-cleanhttp v0.5.0
+	github.com/golang/groupcache v0.0.0-20191027212112-611e8accdfc9 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.1
 	github.com/hashicorp/go-safetemp v1.0.0
-	github.com/hashicorp/go-version v1.1.0
+	github.com/hashicorp/go-version v1.2.0
+	github.com/hashicorp/golang-lru v0.5.3 // indirect
+	github.com/jstemmer/go-junit-report v0.9.1 // indirect
 	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-ieproxy v0.0.0-20191113090002-7c0f6868bffe // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/mattn/go-runewidth v0.0.4 // indirect
-	github.com/mitchellh/go-homedir v1.0.0
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/mitchellh/go-testing-interface v1.0.0
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.2.2 // indirect
-	github.com/ulikunitz/xz v0.5.5
-	google.golang.org/api v0.9.0
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/ulikunitz/xz v0.5.6
+	go.opencensus.io v0.22.2 // indirect
+	golang.org/x/sys v0.0.0-20191113165036-4c7a9d0fe056 // indirect
+	golang.org/x/tools v0.0.0-20191113232020-e2727e816f5a // indirect
+	google.golang.org/api v0.13.0
+	google.golang.org/appengine v1.6.5 // indirect
+	google.golang.org/grpc v1.25.1 // indirect
 	gopkg.in/cheggaaa/pb.v1 v1.0.27 // indirect
+	gopkg.in/yaml.v2 v2.2.5 // indirect
 )
 
 go 1.13
