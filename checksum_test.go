86a19049365e9cc5655797bd9c643bb0c731fb78
Moss <moss@hashicorp.com>
2020-03-17 15:55:50 +0100

    Fix checksum_test format

--- checksum_test.go
+++ checksum_test.go
@@ -12,7 +12,7 @@ func TestClient_ChecksumFromFileWithSubFolder(t *testing.T) {
 	isoURL := "http://hashicorp.com/ubuntu/dists/bionic-updates/main/installer-amd64/current/images/netboot/mini.iso"
 
 	client := Client{}
-	file, err := client.ChecksumFromFile(ctx, httpChecksums.URL + "/sha256-subfolder.sum", isoURL)
+	file, err := client.ChecksumFromFile(ctx, httpChecksums.URL+"/sha256-subfolder.sum", isoURL)
 
 	if err != nil {
 		t.Fatalf("bad: should not have error: %s", err.Error())
@@ -21,4 +21,3 @@ func TestClient_ChecksumFromFileWithSubFolder(t *testing.T) {
 		t.Fatalf("bad: expecting filename ./netboot/mini.iso but was: %s", file.Filename)
 	}
 }
-
