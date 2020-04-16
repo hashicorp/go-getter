2931f375103cbcf8cbbf5005f9fbd56083c60e4b
Moss <moss@hashicorp.com>
2020-04-16 15:17:06 +0200

    Test circleci with samba server

--- get_smb.go
+++ get_smb.go
@@ -3,7 +3,12 @@ package getter
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/url"
+	"os/exec"
+	"regexp"
+	"runtime"
+	"strings"
 )
 
 // SmbGetter is a Getter implementation that will download a module from
@@ -12,11 +17,12 @@ type SmbGetter struct {
 	getter
 }
 
-const pathError = "samba path should contain valid Host and filepath (smb://<host>/<file_path>)"
+const basePathError = "samba path should contain valid Host and filepath (smb://<host>/<file_path>)"
 
+// TODO: validate mode from smb path instead of stat
 func (g *SmbGetter) Mode(ctx context.Context, u *url.URL) (Mode, error) {
 	if u.Host == "" || u.Path == "" {
-		return ModeFile, fmt.Errorf(pathError)
+		return ModeFile, fmt.Errorf(basePathError)
 	}
 	path := "//" + u.Host + u.Path
 	if u.RawPath != "" {
@@ -25,24 +31,91 @@ func (g *SmbGetter) Mode(ctx context.Context, u *url.URL) (Mode, error) {
 	return mode(path)
 }
 
+// TODO: also copy directory
 func (g *SmbGetter) Get(ctx context.Context, req *Request) error {
-	if req.u.Host == "" || req.u.Path == "" {
-		return fmt.Errorf(pathError)
+	hostPath, filePath, err := g.findHostAndFilePath(req)
+	log.Printf("MOSS hostPath %s filepath %s", hostPath, filePath)
+	if err == nil {
+		err = g.smbclientGetFile(hostPath, filePath, req)
 	}
-	path := "//" + req.u.Host + req.u.Path
-	if req.u.RawPath != "" {
-		path = req.u.RawPath
+
+	if err != nil && err.Error() == basePathError {
+		return err
 	}
-	return get(path, req)
+
+	// Look for local mount of shared folder
+	if err != nil && runtime.GOOS == "windows" {
+		err = get(hostPath, req)
+	}
+
+	// throw error msg to install smbclient or mount shared folder
+	return err
 }
 
 func (g *SmbGetter) GetFile(ctx context.Context, req *Request) error {
+	hostPath, filePath, err := g.findHostAndFilePath(req)
+	if err == nil {
+		err = g.smbclientGetFile(hostPath, filePath, req)
+	}
+
+	if err != nil && err.Error() == basePathError {
+		return err
+	}
+
+	// Look for local mount of shared folder
+	if err != nil && runtime.GOOS == "windows" {
+		err = getFile(hostPath, req, ctx)
+	}
+
+	// throw error msg to install smbclient or mount shared folder
+	return err
+}
+
+func (g *SmbGetter) findHostAndFilePath(req *Request) (string, string, error) {
 	if req.u.Host == "" || req.u.Path == "" {
-		return fmt.Errorf(pathError)
+		return "", "", fmt.Errorf(basePathError)
 	}
-	path := "//" + req.u.Host + req.u.Path
-	if req.u.RawPath != "" {
-		path = req.u.RawPath
+	// Host path
+	hostPath := "//" + req.u.Host
+
+	// Get shared directory
+	path := strings.TrimPrefix(req.u.Path, "/")
+	splt := regexp.MustCompile(`/`)
+	directories := splt.Split(path, 2)
+
+	if len(directories) > 0 {
+		hostPath = hostPath + "/" + directories[0]
 	}
-	return getFile(path, req, ctx)
+
+	// Check file path
+	if len(directories) <= 1 || directories[1] == "" {
+		return "", "", fmt.Errorf("can not find file path and/or name in the smb url")
+	}
+
+	return hostPath, directories[1], nil
+}
+
+func (g *SmbGetter) smbclientGetFile(hostPath string, fileDir string, req *Request) error {
+	file := ""
+	if strings.Contains(fileDir, "/") {
+		i := strings.LastIndex(fileDir, "/")
+		file = fileDir[i+1 : len(fileDir)-1]
+		fileDir = fileDir[0:i]
+	} else {
+		file = fileDir
+		fileDir = "."
+	}
+
+	// Get auth user and password
+	auth := req.u.User.Username()
+	if password, ok := req.u.User.Password(); ok {
+		auth = auth + "%" + password
+	}
+
+	// Execute smbclient command
+	getCommand := fmt.Sprintf("'get %s'", file)
+	cmd := exec.Command("smbclient", "-U", auth, hostPath, "--directory", fileDir, "-c", getCommand)
+	log.Printf("Moss BEFORE EXECUTING COMMANDS")
+	cmd.Dir = req.Dst
+	return getRunCommand(cmd)
 }
--- get_smb_test.go
+++ get_smb_test.go
@@ -10,101 +10,114 @@ func TestSmbGetter_impl(t *testing.T) {
 	var _ Getter = new(SmbGetter)
 }
 
+// make the structure
 func TestSmbGetter_Get(t *testing.T) {
 	g := new(SmbGetter)
 	ctx := context.Background()
 
-	// no hostname provided
-	url, err := urlhelper.Parse("smb://")
+	// correct url with auth data
+	url, err := urlhelper.Parse("smb://vagrant:vagrant@samba/shared/file.txt")
 	if err != nil {
 		t.Fatalf("err: %s", err.Error())
 	}
 	req := &Request{
 		u: url,
 	}
-	if err := g.Get(ctx, req); err == nil {
-		t.Fatalf("err: should fail when request url doesn't have a Host")
-	}
-	if err := g.GetFile(ctx, req); err != nil && err.Error() != pathError {
-		t.Fatalf("err: expected error: %s\n but error was: %s", pathError, err.Error())
+	if err := g.Get(ctx, req); err != nil {
+		t.Fatalf("err: should not fail %s", err.Error())
 	}
 
-	// no filepath provided
-	url, err = urlhelper.Parse("smb://host")
+	//correct url with auth data and subdir
+	url, err = urlhelper.Parse("smb://vagrant:vagrant@samba/shared/subdir/file.txt")
 	if err != nil {
 		t.Fatalf("err: %s", err.Error())
 	}
 	req = &Request{
 		u: url,
 	}
-	if err := g.Get(ctx, req); err == nil {
-		t.Fatalf("err: should fail when request url doesn't have a Host")
+	if err := g.Get(ctx, req); err != nil {
+		t.Fatalf("err: should not fail: %s", err.Error())
 	}
-	if err := g.GetFile(ctx, req); err != nil && err.Error() != pathError {
-		t.Fatalf("err: expected error: %s\n but error was: %s", pathError, err.Error())
-	}
-}
-
-func TestSmbGetter_GetFile(t *testing.T) {
-	g := new(SmbGetter)
-	ctx := context.Background()
 
 	// no hostname provided
-	url, err := urlhelper.Parse("smb://")
-	if err != nil {
-		t.Fatalf("err: %s", err.Error())
-	}
-	req := &Request{
-		u: url,
-	}
-	if err := g.Get(ctx, req); err == nil {
-		t.Fatalf("err: should fail when request url doesn't have a Host")
-	}
-	if err := g.GetFile(ctx, req); err != nil && err.Error() != pathError {
-		t.Fatalf("err: expected error: %s\n but error was: %s", pathError, err.Error())
-	}
-
-	// no filepath provided
-	url, err = urlhelper.Parse("smb://host")
-	if err != nil {
-		t.Fatalf("err: %s", err.Error())
-	}
-	req = &Request{
-		u: url,
-	}
-	if err := g.Get(ctx, req); err == nil {
-		t.Fatalf("err: should fail when request url doesn't have a Host")
-	}
-	if err := g.GetFile(ctx, req); err != nil && err.Error() != pathError {
-		t.Fatalf("err: expected error: %s\n but error was: %s", pathError, err.Error())
-	}
-}
-
-func TestSmbGetter_Mode(t *testing.T) {
-	g := new(SmbGetter)
-	ctx := context.Background()
-
-	// no hostname provided
-	url, err := urlhelper.Parse("smb://")
-	if err != nil {
-		t.Fatalf("err: %s", err.Error())
-	}
-	if _, err := g.Mode(ctx, url); err == nil {
-		t.Fatalf("err: should fail when request url doesn't have a Host")
-	}
-	if _, err := g.Mode(ctx, url); err != nil && err.Error() != pathError {
-		t.Fatalf("err: expected error: %s\n but error was: %s", pathError, err.Error())
-	}
-
-	// no filepath provided
 	url, err = urlhelper.Parse("smb://")
 	if err != nil {
 		t.Fatalf("err: %s", err.Error())
 	}
-	if _, err := g.Mode(ctx, url); err == nil {
+	req = &Request{
+		u: url,
+	}
+	if err := g.Get(ctx, req); err == nil {
 		t.Fatalf("err: should fail when request url doesn't have a Host")
 	}
-	if _, err := g.Mode(ctx, url); err != nil && err.Error() != pathError {
-		t.Fatalf("err: expected error: %s\n but error was: %s", pathError, err.Error())
+
+	// no filepath provided
+	url, err = urlhelper.Parse("smb://host")
+	if err != nil {
+		t.Fatalf("err: %s", err.Error())
+	}
+	req = &Request{
+		u: url,
+	}
+	if err := g.Get(ctx, req); err == nil {
+		t.Fatalf("err: should fail when request url doesn't have a Host")
 	}
 }
+
+//func TestSmbGetter_GetFile(t *testing.T) {
+//	g := new(SmbGetter)
+//	ctx := context.Background()
+//
+//	// no hostname provided
+//	url, err := urlhelper.Parse("smb://")
+//	if err != nil {
+//		t.Fatalf("err: %s", err.Error())
+//	}
+//	req := &Request{
+//		u: url,
+//	}
+//	if err := g.GetFile(ctx, req); err != nil && err.Error() != basePathError {
+//		t.Fatalf("err: expected error: %s\n but error was: %s", basePathError, err.Error())
+//	}
+//
+//	// no filepath provided
+//	url, err = urlhelper.Parse("smb://host")
+//	if err != nil {
+//		t.Fatalf("err: %s", err.Error())
+//	}
+//	req = &Request{
+//		u: url,
+//	}
+//	if err := g.GetFile(ctx, req); err != nil && err.Error() != basePathError {
+//		t.Fatalf("err: expected error: %s\n but error was: %s", basePathError, err.Error())
+//	}
+//}
+
+//func TestSmbGetter_Mode(t *testing.T) {
+//	g := new(SmbGetter)
+//	ctx := context.Background()
+//
+//	// no hostname provided
+//	url, err := urlhelper.Parse("smb://")
+//	if err != nil {
+//		t.Fatalf("err: %s", err.Error())
+//	}
+//	if _, err := g.Mode(ctx, url); err == nil {
+//		t.Fatalf("err: should fail when request url doesn't have a Host")
+//	}
+//	if _, err := g.Mode(ctx, url); err != nil && err.Error() != basePathError {
+//		t.Fatalf("err: expected error: %s\n but error was: %s", basePathError, err.Error())
+//	}
+//
+//	// no filepath provided
+//	url, err = urlhelper.Parse("smb://")
+//	if err != nil {
+//		t.Fatalf("err: %s", err.Error())
+//	}
+//	if _, err := g.Mode(ctx, url); err == nil {
+//		t.Fatalf("err: should fail when request url doesn't have a Host")
+//	}
+//	if _, err := g.Mode(ctx, url); err != nil && err.Error() != basePathError {
+//		t.Fatalf("err: expected error: %s\n but error was: %s", basePathError, err.Error())
+//	}
+//}
