eb513d241b2ae890f0ce97c5f92461f78df950d0
Chris Gilmer <chris.gilmer@gmail.com>
2021-05-03 10:44:53 -0700

    Fix a copy-pasta mistake with wording

--- client_option_insecure.go
+++ client_option_insecure.go
@@ -5,7 +5,7 @@ package getter
 // For example, when connecting on HTTPS where an
 // invalid certificate is presented.
 // User assumes all risk.
-// Not all getters have progress support yet.
+// Not all getters have support for insecure mode yet.
 func WithInsecure() func(*Client) error {
 	return func(c *Client) error {
 		c.Insecure = true
