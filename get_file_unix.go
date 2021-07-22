56be771c42dbcd72bd5e8c607c72318d93fd6d70
Erik Bunn <ebu@basen.net>
2021-07-22 10:36:54 +0200

    Added test; reverted parent dir creation; reverted module path

--- get_file_unix.go
+++ get_file_unix.go
@@ -41,7 +41,7 @@ func (g *FileGetter) Get(dst string, u *url.URL) error {
 	}
 
 	// Create all the parent directories
-	if err := os.MkdirAll(dst, g.client.mode(0755)); err != nil {
+	if err := os.MkdirAll(filepath.Dir(dst), g.client.mode(0755)); err != nil {
 		return err
 	}
 
