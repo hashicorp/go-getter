f4436ce60b0ddcc727a77d3c4c5a3e47f5f32e09
Claire <claire@hashicorp.com>
2022-10-27 11:50:22 -0500

    add s3 directory
    update test s3 bucket name
    
    let see if s3 tests work now
    
    try matrix for multiple directories
    
    try to fix GCS tests
    
    move go tests after GCS auth
    
    Revert "move go tests after GCS auth"
    
    This reverts commit 7adfd47169379b9622384292071ff0f11f452c53.
    
    move go tests after GCS auth
    
    comment out other workflows
    
    go mod tidy
    
    go mod tidy
    
    comment out go cmd for now
    
    Revert "go mod tidy"
    
    This reverts commit 416722c36263eccf64952cbcd87333266a5a8ec4.
    
    remove helper directory
    
    revert go mod
    
    add go cmd
    
    go mod tidy for GCS
    
    go mod tidy for cmd

--- s3/get_s3_test.go
+++ s3/get_s3_test.go
@@ -36,7 +36,7 @@ func TestGetter(t *testing.T) {
 	g := new(Getter)
 	dst := testing_helper.TempDir(t)
 	req := &getter.Request{
-		Src:     "s3.amazonaws.com/hc-oss-test/go-getter/folder",
+		Src:     "s3.amazonaws.com/hc-go-getter-test/go-getter/folder",
 		Dst:     dst,
 		GetMode: getter.ModeAny,
 	}
@@ -64,7 +64,7 @@ func TestGetter_subdir(t *testing.T) {
 	g := new(Getter)
 	dst := testing_helper.TempDir(t)
 	req := &getter.Request{
-		Src: "s3::https://s3.amazonaws.com/hc-oss-test/go-getter/folder/subfolder",
+		Src: "s3::https://s3.amazonaws.com/hc-go-getter-test/go-getter/folder/subfolder",
 		Dst: dst,
 	}
 
@@ -94,7 +94,7 @@ func TestGetter_GetFile(t *testing.T) {
 
 	req := &getter.Request{
 		Dst:     dst,
-		Src:     "s3.amazonaws.com/hc-oss-test/go-getter/folder/main.tf",
+		Src:     "s3.amazonaws.com/hc-go-getter-test/go-getter/folder/main.tf",
 		GetMode: getter.ModeFile,
 	}
 
@@ -123,7 +123,7 @@ func TestGetter_GetFile_badParams(t *testing.T) {
 	defer os.RemoveAll(filepath.Dir(dst))
 
 	req := &getter.Request{
-		Src:     "s3.amazonaws.com/hc-oss-test/go-getter/folder/main.tf?aws_access_key_id=foo&aws_access_key_secret=bar&aws_access_token=baz",
+		Src:     "s3.amazonaws.com/hc-go-getter-test/go-getter/folder/main.tf?aws_access_key_id=foo&aws_access_key_secret=bar&aws_access_token=baz",
 		Dst:     dst,
 		GetMode: getter.ModeFile,
 	}
@@ -151,7 +151,7 @@ func TestGetter_GetFile_notfound(t *testing.T) {
 	defer os.RemoveAll(filepath.Dir(dst))
 
 	req := &getter.Request{
-		Src:     "s3.amazonaws.com/hc-oss-test/go-getter/folder/404.tf",
+		Src:     "s3.amazonaws.com/hc-go-getter-test/go-getter/folder/404.tf",
 		Dst:     dst,
 		GetMode: getter.ModeFile,
 	}
@@ -173,7 +173,7 @@ func TestGetter_Mode_dir(t *testing.T) {
 
 	// Check client mode on a key prefix with only a single key.
 	mode, err := g.Mode(ctx,
-		urlhelper.MustParse("https://s3.amazonaws.com/hc-oss-test/go-getter/folder"))
+		urlhelper.MustParse("https://s3.amazonaws.com/hc-go-getter-test/go-getter/folder"))
 	if err != nil {
 		t.Fatalf("err: %s", err)
 	}
@@ -189,7 +189,7 @@ func TestGetter_Mode_file(t *testing.T) {
 
 	// Check client mode on a key prefix which contains sub-keys.
 	mode, err := g.Mode(ctx,
-		urlhelper.MustParse("https://s3.amazonaws.com/hc-oss-test/go-getter/folder/main.tf"))
+		urlhelper.MustParse("https://s3.amazonaws.com/hc-go-getter-test/go-getter/folder/main.tf"))
 	if err != nil {
 		t.Fatalf("err: %s", err)
 	}
@@ -209,7 +209,7 @@ func TestGetter_Mode_notfound(t *testing.T) {
 	// prefix is handled properly (e.g., "/fold" and "/folder" don't put the
 	// client mode into "dir".
 	mode, err := g.Mode(ctx,
-		urlhelper.MustParse("https://s3.amazonaws.com/hc-oss-test/go-getter/fold"))
+		urlhelper.MustParse("https://s3.amazonaws.com/hc-go-getter-test/go-getter/fold"))
 	if err != nil {
 		t.Fatalf("err: %s", err)
 	}
@@ -226,7 +226,7 @@ func TestGetter_Mode_collision(t *testing.T) {
 	// Check that the client mode is "file" if there is both an object and a
 	// folder with a common prefix (i.e., a "collision" in the namespace).
 	mode, err := g.Mode(ctx,
-		urlhelper.MustParse("https://s3.amazonaws.com/hc-oss-test/go-getter/collision/foo"))
+		urlhelper.MustParse("https://s3.amazonaws.com/hc-go-getter-test/go-getter/collision/foo"))
 	if err != nil {
 		t.Fatalf("err: %s", err)
 	}
