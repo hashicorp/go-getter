cf15d8405d9a700e9c25df7194d095b7dfdff914
Kent 'picat' Gruber <kent@hashicorp.com>
2023-02-08 09:48:46 -0500

    Add decompression bomb mitigation options

--- decompress_zip_test.go
+++ decompress_zip_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"testing"
 )
 
@@ -128,4 +129,26 @@ func TestDecompressZipPermissions(t *testing.T) {
 
 	expected["directory/setuid"] = masked
 	testDecompressorPermissions(t, d, input, expected, os.FileMode(060000000))
+}
+
+func TestDecompressZipBomb(t *testing.T) {
+	// If the zip decompression bomb protection fails, this can fill up disk space on the entire
+	// computer.
+	if os.Getenv("GO_GETTER_TEST_ZIP_BOMB") != "true" {
+		t.Skip("skipping potentially dangerous test without GO_GETTER_TEST_ZIP_BOMB=true")
+	}
+
+	// https://www.bamsoftware.com/hacks/zipbomb/zblg.zip
+	srcPath := filepath.Join("./testdata", "decompress-zip", "bomb.zip")
+
+	d := new(ZipDecompressor)
+	d.FileSizeLimit = 512
+
+	err := d.Decompress(t.TempDir(), srcPath, true, 0644)
+	if err == nil {
+		t.FailNow()
+	}
+	if !strings.Contains(err.Error(), "zip archive larger than limit: 512") {
+		t.Fatalf("unexpected error: %q", err.Error())
+	}
 }
