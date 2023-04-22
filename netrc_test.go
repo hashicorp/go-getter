5ccb39a79407cabe43d0f263e9358614acaa0cda
Adrian Ulrich <adrian@blinkenlights.ch>
2023-04-22 18:23:09 +0200

    Make addAuthFromNetrc ignore ENOTDIR errors
    The function already returns early if the specified 'netrc' configuration points to a non existing file, but currently returns an error if the OS reports ENOTDIR:
    
    This will happen if the $HOME directory of the specified user points to a file instead a directory - something eg. 'void linux' does for user 'nobody':
    
    ```
    $ grep nobody /etc/passwd
    nobody:x:99:99:Unprivileged User:/dev/null:/bin/false
    ```
    
    go-getter then attempts to open `/dev/null/.netrc` which fails with ENOTDIR - something that should just be threated the same way as a non existing file (in this case)

--- netrc_test.go
+++ netrc_test.go
@@ -61,3 +61,43 @@ func TestAddAuthFromNetrc_hasUsername(t *testing.T) {
 		t.Fatalf("Mismatch: %q != %q", actual, expected)
 	}
 }
+
+func TestAddAuthFromNetrc_isNotExist(t *testing.T) {
+	defer tempEnv(t, "NETRC", "./testdata/netrc/_does_not_exist")()
+
+	u, err := url.Parse("http://example.com")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if err := addAuthFromNetrc(u); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// no netrc, no change:
+	expected := "http://example.com"
+	actual := u.String()
+	if expected != actual {
+		t.Fatalf("Mismatch: %q != %q", actual, expected)
+	}
+}
+
+func TestAddAuthFromNetrc_isNotADirectory(t *testing.T) {
+	defer tempEnv(t, "NETRC", "./testdata/netrc/basic/parent-not-a-dir")()
+
+	u, err := url.Parse("http://example.com")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if err := addAuthFromNetrc(u); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// no netrc, no change:
+	expected := "http://example.com"
+	actual := u.String()
+	if expected != actual {
+		t.Fatalf("Mismatch: %q != %q", actual, expected)
+	}
+}
