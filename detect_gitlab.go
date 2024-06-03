5515e0a1ab1760180d1f785e7310572365aab7b8
tvizor <eugene.zenovych@gmail.com>
2024-06-03 13:09:46 -0700

    fix: #479 allow nested repos for gitlab.com

--- detect_gitlab.go
+++ detect_gitlab.go
@@ -23,25 +23,29 @@ func (d *GitLabDetector) Detect(src, _ string) (string, bool, error) {
 }
 
 func (d *GitLabDetector) detectHTTP(src string) (string, bool, error) {
-	parts := strings.Split(src, "/")
-	if len(parts) < 3 {
-		return "", false, fmt.Errorf(
-			"GitLab URLs should be gitlab.com/username/repo")
-	}
-
-	urlStr := fmt.Sprintf("https://%s", strings.Join(parts[:3], "/"))
-	repoUrl, err := url.Parse(urlStr)
+	repoUrl, err := url.Parse(fmt.Sprintf("https://%s", src))
 	if err != nil {
 		return "", true, fmt.Errorf("error parsing GitLab URL: %s", err)
 	}
 
-	if !strings.HasSuffix(repoUrl.Path, ".git") {
-		repoUrl.Path += ".git"
+	parts := strings.Split(repoUrl.Path, "//")
+
+	if len(strings.Split(parts[0], "/")) < 3 {
+		return "", false, fmt.Errorf(
+			"GitLab URLs should be gitlab.com/username/repo " +
+				"or gitlab.com/organization/project/repo")
 	}
 
-	if len(parts) > 3 {
-		repoUrl.Path += "//" + strings.Join(parts[3:], "/")
+	if len(parts) > 2 {
+		return "", false, fmt.Errorf(
+			"URL malformed: \"//\" can only used once in path")
 	}
+
+	if !strings.HasSuffix(parts[0], ".git") {
+		parts[0] += ".git"
+	}
+
+	repoUrl.Path = fmt.Sprintf("%s", strings.Join(parts, "//"))
 
 	return "git::" + repoUrl.String(), true, nil
 }
