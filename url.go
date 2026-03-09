f4a23435b04a7c677ea6c2b1b3285d31e973d822
dduzgun-security <deniz.duzgun@hashicorp.com>
2026-03-09 19:36:25 -0400

    sec: bump go and xrepos + redact aws tokens in url

--- url.go
+++ url.go
@@ -5,11 +5,21 @@ package getter
 
 import "net/url"
 
+// redactedParams is the list of URL query parameter names whose values are
+// sensitive and must be replaced with "redacted" in error messages and logs.
+var redactedParams = []string{
+	"sshkey",
+	"aws_access_key_id",
+	"aws_access_key_secret",
+	"aws_access_token",
+}
+
 // RedactURL is a port of url.Redacted from the standard library,
 // which is like url.String but replaces any password with "redacted".
 // Only the password in u.URL is redacted. This allows the library
 // to maintain compatibility with go1.14.
-// This port was also extended to redact all "sshkey" from URL query parameter
+// This port was also extended to redact sensitive URL query parameters
+// (sshkey, aws_access_key_id, aws_access_key_secret, aws_access_token)
 // and replace them with "redacted".
 func RedactURL(u *url.URL) string {
 	if u == nil {
@@ -21,11 +31,17 @@ func RedactURL(u *url.URL) string {
 		ru.User = url.UserPassword(ru.User.Username(), "redacted")
 	}
 	q := ru.Query()
-	if q.Has("sshkey") {
-		values := q["sshkey"]
-		for i := range values {
-			values[i] = "redacted"
+	modified := false
+	for _, param := range redactedParams {
+		if q.Has(param) {
+			values := q[param]
+			for i := range values {
+				values[i] = "redacted"
+			}
+			modified = true
 		}
+	}
+	if modified {
 		ru.RawQuery = q.Encode()
 	}
 	return ru.String()
