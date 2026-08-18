7de2140c62105416d17a036e67d3259a5654accd
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2026-08-18 22:45:37 +1000

    Add a -version flag to the cli. (#673)

--- version/version.go
+++ version/version.go
@@ -3,9 +3,29 @@
 
 package version
 
+import (
+	"bytes"
+	"fmt"
+)
+
 var (
 	Version           = "1.8.9"
 	VersionPrerelease = "dev"
 	VersionMetadata   = ""
 	// PluginVersion removed to avoid import cycle
 )
+
+func FullVersionString() string {
+	var versionString bytes.Buffer
+
+	fmt.Fprintf(&versionString, "Go-Getter v%s", Version)
+	if VersionPrerelease != "" {
+		fmt.Fprintf(&versionString, "-%s", VersionPrerelease)
+	}
+
+	if VersionMetadata != "" {
+		fmt.Fprintf(&versionString, "+%s", VersionMetadata)
+	}
+
+	return versionString.String()
+}
