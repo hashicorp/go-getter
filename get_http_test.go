b5b384fa8171e86d9c138f751e99f38273214ea7
Juanadelacuesta <8647634+Juanadelacuesta@users.noreply.github.com>
2026-09-03 13:57:37 +0200

    fix: remove extra : from test url

--- get_http_test.go
+++ get_http_test.go
@@ -776,7 +776,7 @@ func testHttpServerWithXTerraformGetLoop(t *testing.T) net.Listener {
 		t.Fatalf("err: %s", err)
 	}
 
-	header := fmt.Sprintf("http://%v:%v", ln.Addr().String(), "/loop")
+	header := fmt.Sprintf("http://%v%v", ln.Addr().String(), "/loop")
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/loop", func(w http.ResponseWriter, r *http.Request) {
